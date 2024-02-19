64ed9ef42a2782d76030ffabf870e65127e5c1b9
fefei69 <coolpixs04@gmail.com>
2024-02-19 05:12:37 -0800

    icp works but not good

--- code/icp_warm_up/test_icp.py
+++ code/icp_warm_up/test_icp.py
@@ -1,22 +1,25 @@
 
 import numpy as np
-from utils import read_canonical_model, load_pc, visualize_icp_result
-
+from utils import read_canonical_model, load_pc, data_association, visualize_icp_result, icp
+from scipy.spatial.transform import Rotation
 if __name__ == "__main__":
   obj_name = 'drill' # drill or liq_container
   num_pc = 1 # number of point clouds
-
+  down_sample_rate = 1
   source_pc = read_canonical_model(obj_name)
-  source_pc_mean = source_pc - np.mean(source_pc)
+  # A point at origin for testing
+  zero = np.zeros((1,3))
   # print(source_pc_mean)
   for i in range(num_pc):
     target_pc = load_pc(obj_name, i)
     diff_center = np.mean(source_pc,axis=0,keepdims=True) - np.mean(target_pc,axis=0,keepdims=True)
-    target_pc = target_pc + diff_center
-
+    target = target_pc + diff_center
+    R_optimal = icp(source_pc, target_pc, down_sample_rate)
+    # Rot = Rotation.from_euler('z',-1).as_matrix()
+    # R_optimal = target @ Rot.T
+    # optimal_target_pc = target @ R_optimal.T
     # estimated_pose, you need to estimate the pose with ICP
     pose = np.eye(4)
-
     # visualize the estimated result
-    visualize_icp_result(source_pc, target_pc, pose)
+    visualize_icp_result(source_pc, R_optimal, pose)
 
--- code/icp_warm_up/utils.py
+++ code/icp_warm_up/utils.py
@@ -2,7 +2,65 @@ import os
 import scipy.io as sio
 import numpy as np
 import open3d as o3d
+from sklearn.neighbors import NearestNeighbors
+from scipy.spatial.transform import Rotation
+def loss(source, target, R):
+  loss = np.sum(np.linalg.norm(target @ R.T - source, axis=0,keepdims=True))
+  return loss
 
+def Kabsch_Algorithm(source, target):
+  Q_matrix = np.zeros((3,3))
+  for i in range(source.shape[0]):
+    Q_matrix = Q_matrix + source[i] @ target[i].T
+  U, S, V_T = np.linalg.svd(Q_matrix)
+  # avoid reflection
+  F = np.array([[1, 0, 0], 
+                [0, 1, 0], 
+                [0, 0, np.linalg.det(U @ V_T)]])
+  R_optimal = U @ F @ V_T
+  return R_optimal
+
+def icp(source, target, down_sample_rate, max_iterations=100, tolerance=1e-5):
+  '''
+  Iterative Closest Point (ICP) algorithm
+  source: numpy array, (N, 3)
+  target: numpy array, (N, 3)
+  max_iterations: int, maximum number of iterations
+  tolerance: float, difference in error to stop iterations
+  return: SO(3) numpy array, (3, 3), Rotation matrix
+  '''
+  diff_center = np.mean(source,axis=0,keepdims=True) - np.mean(target,axis=0,keepdims=True)
+  if down_sample_rate == 1:
+    source_pc_downsampled = source
+  else:
+    source_pc_downsampled = source[::int(down_sample_rate/5)]
+  target = target + diff_center
+  target_pc_downsampled = target[::down_sample_rate]
+  # Sample random z axis rotation for initial guess
+  # random_rot = Rotation.random().as_euler('zyx')
+  # Rot = Rotation.from_euler('z', random_rot[0]).as_matrix()
+  Rot = Rotation.from_euler('z',-1).as_matrix()
+  rot_target_pc_downsampled =  target_pc_downsampled @ Rot.T
+  target_pc_downsampled_original = np.copy(target_pc_downsampled)
+  for i in range(max_iterations):
+    # Find the nearest neighbors
+    associated_source, target_pc = data_association(source_pc_downsampled, rot_target_pc_downsampled)
+    R = Kabsch_Algorithm(associated_source, target_pc)
+    print("ICP iteration: ",i)
+    LOSS = loss(associated_source, target_pc, R)
+    print("Loss: ",LOSS)
+    if LOSS < 2:
+      break
+    rot_target_pc_downsampled =  target_pc @ R.T
+
+  return rot_target_pc_downsampled
+
+def data_association(source_pc, target_pc):
+  print("Numbers of Neighbors",target_pc.shape[0])
+  neighbbors = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(source_pc)
+  distances, indices = neighbbors.kneighbors(target_pc)
+  assoc_source = source_pc[indices].reshape(-1,3)
+  return assoc_source, target_pc
 
 def read_canonical_model(model_name):
   '''
